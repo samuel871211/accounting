024e0ca108f5eb48a2ff9dac3babb3d40051ddf8
shemira <samuel871211@gmail.com>
2022-02-14 17:04:13 +0800

    chore: 加入sass

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: { host: '0.0.0.0' }
+  server: { host: '0.0.0.0' },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        /* Specify options to pass to SCSS pre-processors. */
+        additionalData: `
+          @import "./assets/style/variables.scss";
+        `,
+      },
+    },
+  },
 })
